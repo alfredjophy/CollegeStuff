3475d46279e3fc4e97fd8757ce43103041269a44
Alfred Jophy <alfredjophy@protonmail.com>
2021-07-31 07:08:32 +0530

    .

---
+++ Sem_3_Data_Structures/others/KMP_algorithm/kmp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+        
+        char str[]="abcdaabaabbabdff";
+        char ptrn[]="abb";
+        
+        int T[strlen(ptrn)];
+
+        //create partial match table
+        int start=0,end=0;
+
+        while(end<strlen(ptrn)){
+                if(ptrn[start]==ptrn[end]){
+
+                }
+                else{
+                        
+                }
+        }
+
+
+        //
+
+        return 0;
+}
